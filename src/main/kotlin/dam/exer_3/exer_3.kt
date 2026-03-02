21a4191da63eb709106545c980f524d8ef3f43b0
Rafael Galveia Taveira David Faustino <rgtdfaustino@gmail.com>
2026-03-02 16:31:30 +0000

    exercise 3 completed

---
+++ src/main/kotlin/dam/exer_3/exer_3.kt
@@ -0,0 +1,26 @@
+package org.example.dam.exer_3
+
+fun main() {
+    val currentHeight = 100f
+    val newHeightPercent = 0.6f
+    val minHeight = 1f
+    val maxQualifyingBounces = 15
+
+    val bounces = generateSequence(currentHeight) { previousHeight ->
+        bounceBall(previousHeight, newHeightPercent, minHeight)
+    }
+
+    // o take apanha os primeiros n elementos ou seja primeiros 15
+    val firstBounces = bounces.take(maxQualifyingBounces).toList()
+    println(firstBounces)
+}
+
+// precisamos do ponto de interrogação porque o valor retornado pode ser null, não é sempre float
+fun bounceBall(previousHeight: Float, newHeightPercent: Float, minHeight: Float): Float? {
+    val newHeight = previousHeight * newHeightPercent
+
+    if (newHeight >= minHeight)
+        return newHeight
+    else
+        return null
+}
