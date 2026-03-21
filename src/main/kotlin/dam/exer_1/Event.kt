c8ecb5d7031dc2ef5ba0b60d8faa7de900ab5a81
Rafael Galveia Taveira David Faustino <rgtdfaustino@gmail.com>
2026-03-21 14:23:38 +0000

    content added

--- src/main/kotlin/dam/exer_1/Event.kt
+++ src/main/kotlin/dam/exer_1/Event.kt
@@ -1,29 +1,68 @@
 package org.example.dam.exer_1
 
+import org.example.dam.exer_1.Event.*
+
+
 sealed class Event {
-    class Login(val username: String, val timestamp: Long){
 
+    class Login(val u: String, val ts: Long) : Event() {
+        val username = u
+        val timestamp = ts
     }
 
-    class Purchase(val username: String, val amount: Double, val timestamp: Long){
-
+    class Purchase(val u: String, val a: Double, val ts: Long) : Event(){
+        val username = u
+        val amount = a
+        val timestamp = ts
     }
 
 
-    class Logout(val username: String, val timestamp: Long){
+    class Logout(val u: String, val ts: Long) : Event(){
+        val username = u
+        val timestamp = ts
 
     }
+}
 
-    fun List<Event>.filterByUser(parameter: String){
-        return
+
+fun List<Event>.filterByUser(parameter: String): List<Event> {
+    val list = mutableListOf<Event>()
+
+    for(i in 0 .. this.size - 1) {
+        if ((this.get(i) is Login) && (this.get(i) as Login).username == parameter)
+            list.add(this.get(i))
+        if ((this.get(i) is Purchase) && (this.get(i) as Purchase).username == parameter)
+            list.add(this.get(i))
+        if ((this.get(i) is Logout) && (this.get(i) as Logout).username == parameter)
+            list.add(this.get(i))
     }
 
-    fun List<Event>.totalSpent(parameter: String){
-        return
+    println("Events for $parameter:")
+    for (event in list) {
+        when (event) {
+            is Login -> println("Login (username = ${event.username}, timestamp = ${event.timestamp})")
+            is Purchase -> println("Purchase (username = ${event.username}, amount = ${event.amount}, timestamp = ${event.timestamp})")
+            is Logout -> println("Logout (username = ${event.username}, timestamp = ${event.timestamp})")
+        }
     }
 
-    fun List<Event>.processEvents(handler: (Event) -> Unit): Unit {
+    return list
+}
 
+fun List<Event>.totalSpent(parameter: String): Double{
+    var total = 0.0
+
+    for(i in 0 .. this.size - 1) {
+        if ((this.get(i) is Purchase) && (this.get(i) as Purchase).username == parameter)
+            total += (this.get(i) as Purchase).amount
     }
 
+    return total
+}
+
+
+fun List<Event>.processEvents(handler: (Event) -> Unit) {
+    for (event in this) {
+        handler(event)
+    }
 }
