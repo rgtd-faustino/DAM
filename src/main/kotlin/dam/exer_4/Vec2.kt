61d3d0420bfe0a8bd25f568769f21b14122bf064
Rafael Galveia Taveira David Faustino <rgtdfaustino@gmail.com>
2026-03-23 13:48:54 +0000

    exercise 1.4 structure completed

---
+++ src/main/kotlin/dam/exer_4/Vec2.kt
@@ -0,0 +1,40 @@
+package dam.exer_4
+
+
+data class Vec2(val x: Double, val y: Double) {
+    operator fun plus(value: Vec2): Vec2 {
+
+    }
+
+    operator fun minus(value: Vec2): Vec2 {
+
+    }
+
+    operator fun times(value: Double): Vec2 {
+
+    }
+
+    operator fun unaryMinus(): Vec2 {
+
+    }
+
+    operator fun compareTo(value: Vec2): Int {
+
+    }
+
+    fun magnitude(): Double {
+
+    }
+
+    fun dot(value: Vec2): Double {
+
+    }
+
+    fun normalized(): Vec2 {
+
+    }
+
+    operator fun get(index: Int): Double {
+
+    }
+}
