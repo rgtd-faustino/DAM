6f1b09d74dbadf7788a1c55e3a29844bc7fdf923
Rafael Galveia Taveira David Faustino <rgtdfaustino@gmail.com>
2026-04-11 03:15:00 +0100

    added themes, colors and fixed layouts for the new day/night background switching

--- app/src/main/java/dam/A51394/coolweatherapp/MainActivity.kt
+++ app/src/main/java/dam/A51394/coolweatherapp/MainActivity.kt
@@ -1,20 +1,32 @@
 package dam.A51394.coolweatherapp
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private var day = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // este código não estava a mudar nada, quando fui ver apercebi-me que o layout que uso
+        // tem que o background fica por cima do windowBackground, então a solução seria ou
+        // mudar diretamente o fundo (mas assim não eram usados os themes) ou então remover os
+        // backgrounds dos layouts e deixar que o código trate disso, escolhi a solução 2
+        when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_PORTRAIT -> {
+                if (day) setTheme(R.style.Theme_Day)
+                else setTheme(R.style.Theme_Night)
+            }
+            Configuration.ORIENTATION_LANDSCAPE -> {
+                if (day) setTheme(R.style.Theme_Day_Land)
+                else setTheme(R.style.Theme_Night_Land)
+            }
         }
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
