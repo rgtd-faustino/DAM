9a3370b59dac44670c331128767ccb24aa673554
Rafael Galveia Taveira David Faustino <rgtdfaustino@gmail.com>
2026-04-30 15:45:42 +0100

    step 3 completed

---
+++ processor/src/main/kotlin/processor.kt
@@ -0,0 +1,97 @@
+package processor
+
+import annotations.Greeting
+import com.google.auto.service.AutoService
+import com.squareup.kotlinpoet.*
+import java.io.File
+import javax.annotation.processing.*
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.ExecutableElement
+import javax.lang.model.element.TypeElement
+import javax.tools.Diagnostic
+
+@AutoService(Processor::class)
+@SupportedSourceVersion(SourceVersion.RELEASE_20) // como o 23 dava erro fui descendo até um release que não desse erro
+@SupportedAnnotationTypes("annotations.Greeting")
+class GreetingProcessor : AbstractProcessor() {
+
+    override fun process(
+        annotations: MutableSet<out TypeElement>,
+        roundEnv: RoundEnvironment
+    ): Boolean {
+        val classMethodMap = mutableMapOf<TypeElement, MutableList<ExecutableElement>>()
+
+        for (element in roundEnv.getElementsAnnotatedWith(Greeting::class.java)) {
+            if (element is ExecutableElement) {
+                val enclosingClass = element.enclosingElement as TypeElement
+                classMethodMap.computeIfAbsent(enclosingClass) { mutableListOf() }.add(element)
+            }
+        }
+
+        for ((classElement, methods) in classMethodMap) {
+            generateKotlinWrapperClass(classElement, methods)
+        }
+
+        return true
+    }
+
+    private fun generateKotlinWrapperClass(
+        classElement: TypeElement,
+        methods: List<ExecutableElement>
+    ) {
+        val packageName = processingEnv.elementUtils.getPackageOf(classElement).toString()
+        val originalClassName = classElement.simpleName.toString()
+        val wrapperClassName = "${originalClassName}Wrapper"
+
+        val classBuilder = TypeSpec.classBuilder(wrapperClassName)
+            .primaryConstructor(
+                FunSpec.constructorBuilder()
+                    .addParameter("original", ClassName(packageName, originalClassName))
+                    .build()
+            )
+            .addProperty(
+                PropertySpec.builder("original", ClassName(packageName, originalClassName))
+                    .initializer("original")
+                    .build()
+            )
+            .addModifiers(KModifier.PUBLIC, KModifier.FINAL)
+
+        for (method in methods) {
+            val methodName = method.simpleName.toString()
+            val parameters = method.parameters.map { param ->
+                ParameterSpec.builder(param.simpleName.toString(), param.asType().asTypeName()).build()
+            }
+            val arguments = method.parameters.joinToString(", ") { it.simpleName.toString() }
+            val greetingMessage = method.getAnnotation(Greeting::class.java)?.message ?: "Hello!"
+
+            val methodBuilder = FunSpec.builder(methodName)
+                .addModifiers(KModifier.PUBLIC, KModifier.FINAL)
+                .addParameters(parameters)
+                .addStatement("println(%S)", greetingMessage)
+                .addStatement("original.$methodName($arguments)")
+
+            classBuilder.addFunction(methodBuilder.build())
+        }
+
+        val file = FileSpec.builder(packageName, wrapperClassName)
+            .addType(classBuilder.build())
+            .build()
+
+        try {
+            val kaptKotlinGeneratedDir = processingEnv.options["kapt.kotlin.generated"]
+            if (kaptKotlinGeneratedDir != null) {
+                file.writeTo(File(kaptKotlinGeneratedDir))
+            } else {
+                processingEnv.messager.printMessage(
+                    Diagnostic.Kind.ERROR,
+                    "kapt.kotlin.generated not found"
+                )
+            }
+        } catch (e: Exception) {
+            processingEnv.messager.printMessage(
+                Diagnostic.Kind.ERROR,
+                "Error generating Kotlin file: ${e.message}"
+            )
+        }
+    }
+}
